198225107c0cc8a15e9bbac24afa2ef281e79973
inseoung <thonam123@naver.com>
2026-09-24 05:19:05 +0900

    WIP : Spirng Security Configuration

--- src/main/java/com/onboarding/projects/config/SecurityConfig.java
+++ src/main/java/com/onboarding/projects/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.onboarding.projects.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,13 +12,19 @@ public class SecurityConfig {
 	    @Bean
 	    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		        http
+		        	// CSRF 방지 기능 비활성화
 		            .csrf(csrf -> csrf.disable())
 		            .authorizeHttpRequests(auth -> auth
 		                .requestMatchers(
 		                    "/swagger-ui/**",
-		                    "/v3/api-docs/**"
+		                    "/v3/api-docs/**",
+		                    "/api/v2/auth/login"
 		                ).permitAll()
-		                .anyRequest().permitAll()
+		                .requestMatchers(HttpMethod.POST, "/api/v2/user").permitAll()
+		                .requestMatchers(HttpMethod.DELETE, "/api/v2/user/{userId}").hasRole("ADMIN")
+		                // 매 요청마다 인증
+		                //.anyRequest().permitAll()
+		                .anyRequest().authenticated()
 		            );
 		
 		        return http.build();
